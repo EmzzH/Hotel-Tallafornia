f3c4c12df5e18a9a01b2deb2da0f4ee4d29bd2de
SamJones56 <145762915+SamJones56@users.noreply.github.com>
2024-02-07 10:04:55 +0000

    Initial upload of some files

---
+++ Projectv01/layout/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Header data -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Tallafornia - Home</title>
+    <!-- Nav bar used in Web Development last year -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<!-- Body data -->
+<body>
+<!-- Creating the nav bar -->
+<div class="boxNav">
+    <div class="topnav" id="myTopnav">
+        <a href="index.php" class="active">Home</a>
+        <a href="booking.php">Bookings</a>
+        <a href="restaurant .php">Restaurant </a>
+        <!-- On click drop down menu -->
+        <a href="javascript:void(0);" class="icon" onclick="respNav()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+</body>
